e2470d27d84d3aed6d5776751a174bf989b35a67
happymondaze <happymondazeband@gmail.com>
2016-08-05 11:20:59 +0100

    a

---
+++ js/expanding.content.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$(".moredetail").hide();
+	$('.moredetail').removeClass('hide')
+
+	$(".expand").click(function(){
+				$(this).next(".moredetail").slideToggle(200,"swing");
+			$(this).toggleClass("open");
+			return false;
+		});
+  });
+// Hide the extra content initially, using JS so that if JS is disabled, no problemo:
+$('.read-more-content').addClass('hide')
+$('.read-more-show, .read-more-hide').removeClass('hide')
+// Set up the toggle effect:
+$('.read-more-show').on('click', function(e) {
+  $(this).next('.read-more-content').removeClass('hide');
+  $(this).addClass('hide');
+  e.preventDefault();
+});
+// Changes contributed by @diego-rzg
+$('.read-more-hide').on('click', function(e) {
+  var p = $(this).parent('.read-more-content');
+  p.addClass('hide');
+  p.prev('.read-more-show').removeClass('hide'); // Hide only the preceding "Read More"
+  e.preventDefault();
+});
